cdefaa3ad29a9cfea09cdf1937e98e8d3570526f
frederic.marchais <frederic.marchais@429a5124-debd-11dd-993c-4d20d8c3c59d>
2009-02-13 19:46:24 +0000

    - Added password check when team joins event. - Prettier events matches list display.

--- trunk/eventmatchs.php
+++ trunk/eventmatchs.php
@@ -82,14 +82,30 @@ else
         /* Display table contents */
         $text .= "<table class=\"fborder\" style=\"width:95%\"><tbody>";
         $text .= "<tr><td class=\"forumheader\" style=\"width:120px\"><b>Match ID</b></td><td class=\"forumheader\" style=\"width:90px\"><b>Reported By</b></td><td class=\"forumheader\"><b>Players</b></td><td class=\"forumheader\" style=\"width:90px\"><b>Date</b></td></tr>\n";
-        for($i=0; $i<$num_rows; $i++){
+        for($i=0; $i<$num_rows; $i++)
+        {
             $mID  = mysql_result($result,$i, TBL_MATCHS.".MatchID");
-            $mReportedBy  = mysql_result($result,$i, TBL_MATCHS.".ReportedBy");
-            $mReportedByNickname  = mysql_result($result,$i, TBL_USERS.".user_name");
+            $mReportedBy  = mysql_result($result,$i, TBL_USERS.".user_id");
+            $mReportedByNickName  = mysql_result($result,$i, TBL_USERS.".user_name");
             $mTime  = mysql_result($result,$i, TBL_MATCHS.".TimeReported");
             $mTime_local = $mTime + GMT_TIMEOFFSET;
-            //$date = date("d M Y, h:i:s A",$mTime);
-            $date = date("d M Y",$mTime_local);
+            $date = date("d M Y, h:i:s A",$mTime_local);
+            $q2 = "SELECT DISTINCT ".TBL_MATCHS.".*, "
+            .TBL_SCORES.".Player_Rank"
+            ." FROM ".TBL_MATCHS.", "
+            .TBL_SCORES
+            ." WHERE (".TBL_MATCHS.".MatchID = '$mID')"
+            ." AND (".TBL_SCORES.".MatchID = ".TBL_MATCHS.".MatchID)";
+            $result2 = $sql->db_Query($q2);
+            $numRanks = mysql_numrows($result2);
+            if ($numRanks == 2)
+            {
+                $str = " defeated ";
+            }
+            else
+            {
+                $str = " vs ";
+            }
 
             $q2 = "SELECT ".TBL_MATCHS.".*, "
             .TBL_SCORES.".*, "
@@ -106,14 +122,16 @@ else
             ." ORDER BY ".TBL_SCORES.".Player_Rank";
 
             $result2 = $sql->db_Query($q2);
-            $num_rows2 = mysql_numrows($result2);
+            $numPlayers = mysql_numrows($result2);
             $pname = '';
             $players = '';
-            for($j=0; $j<$num_rows2; $j++)
+
+            $rank = 1;
+            for ($index = 0; $index < $numPlayers; $index++)
             {
-                $pid  = mysql_result($result2,$j, TBL_USERS.".user_id");
-                $pname  = mysql_result($result2,$j, TBL_USERS.".user_name");
-                $pteam  = mysql_result($result2,$j, TBL_PLAYERS.".Team");
+                $pid  = mysql_result($result2,$index , TBL_USERS.".user_id");
+                $pname  = mysql_result($result2,$index , TBL_USERS.".user_name");
+                $pteam  = mysql_result($result2,$index , TBL_SCORES.".Player_Rank");
                 $pclan = '';
                 $pclantag = '';
                 if ($etype == "Team Ladder")
@@ -135,15 +153,22 @@ else
                         $pclantag  = mysql_result($result_3,0, TBL_CLANS.".Tag") ."_";
                     }
                 }
-
-                if ($j==0)
-                $players = "<a href=\"".e_PLUGIN."ebattles/userinfo.php?user=$pid\">$pclantag$pname</a>";
-                else
-                $players = $players.", <a href=\"".e_PLUGIN."ebattles/userinfo.php?user=$pid\">$pclantag$pname</a>";
-            }
-
+                if($index>0)
+                {
+                    if ($pteam == $rank)
+                    {
+                        $players .= " & ";
+                    }
+                    else
+                    {
+                        $players .= $str;
+                        $rank++;
+                    }
+                }
+                $players .= "<a href=\"".e_PLUGIN."ebattles/userinfo.php?user=$pid\">$pclantag$pname</a>";
+              }
             $text .= "<tr>\n";
-            $text .= "<td class=\"forumheader3\"><b>$mID</b> <a href=\"".e_PLUGIN."ebattles/matchinfo.php?eventid=$event_id&amp;matchid=$mID\">(Show details)</a></td><td class=\"forumheader3\"><a href=\"".e_PLUGIN."ebattles/userinfo.php?user=$mReportedBy\">$mReportedByNickname</a></td><td class=\"forumheader3\">$players</td><td class=\"forumheader3\">$date</td></tr>";
+            $text .= "<td class=\"forumheader3\"><b>$mID</b> <a href=\"".e_PLUGIN."ebattles/matchinfo.php?eventid=$event_id&amp;matchid=$mID\">(Show details)</a></td><td class=\"forumheader3\"><a href=\"".e_PLUGIN."ebattles/userinfo.php?user=$mReportedBy\">$mReportedByNickName</a></td><td class=\"forumheader3\">$players</td><td class=\"forumheader3\">$date</td></tr>";
 
 
         }
