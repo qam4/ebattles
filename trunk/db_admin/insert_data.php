d1b694c494959437087d4f8871267a5f01d5bf63
frederic.marchais <frederic.marchais@429a5124-debd-11dd-993c-4d20d8c3c59d>
2009-03-24 20:35:12 +0000

    - Added code to insert event with many players for debug. - Event Manage   . Added points per win/draw/loss.   . Fixed stats sliders indexing. - Event stats update (players/teams)   . Fixed rank calculation (rank increases only if player is ranked)   . Fixed index in *_scores tables, which is populated only for ranked players.   . TrueSkill is shown as a positive integer.     With default settings, 99% of players will have a skill between 0 and 50.   . better init arrays - TinyMce only used for specific textareas with class="mceEditor"

--- trunk/db_admin/insert_data.php
+++ trunk/db_admin/insert_data.php
@@ -272,6 +272,79 @@
 	 VALUES ('$last_id', 'Skill', '1', '20')";
 	$sql->db_Query($query) or die ('Error, adding StatsCategories 1<br />'. mysql_error());
 	
+/*	
+	// Event 4 - 1v1 ladder - 1000 players test
+	$query = 
+	"INSERT INTO ".TBL_EVENTS."(Name,Password,Game,Type,Owner, Start_timestamp, End_timestamp, Rules, Description, AllowDraw, AllowScore)
+	 VALUES ('1v1 Ladder Test - many players', '', '2', 'One Player Ladder',1, $time, ($time+7*86400), '...', '<p>This is a 1v1 test ladder with 1000 player.</p><p>Feel free to join this event and use the &quot;Match Report&quot; system. </p>',1,1)";
+	$sql->db_Query($query) or die ('Error, adding event 4<br />'. mysql_error());
+	
+    $last_id = mysql_insert_id();
+ 	$query = 
+	"INSERT INTO ".TBL_STATSCATEGORIES."(Event, CategoryName, CategoryMinValue, CategoryMaxValue)
+	 VALUES ('$last_id', 'ELO', '1', '20')";
+	$sql->db_Query($query) or die ('Error, adding StatsCategories 1<br />'. mysql_error());
+ 	$query = 
+	"INSERT INTO ".TBL_STATSCATEGORIES."(Event, CategoryName, CategoryMinValue, CategoryMaxValue)
+	 VALUES ('$last_id', 'GamesPlayed', '1', '40')";
+	$sql->db_Query($query) or die ('Error, adding StatsCategories 2<br />'. mysql_error());
+ 	$query = 
+	"INSERT INTO ".TBL_STATSCATEGORIES."(Event, CategoryName, CategoryMinValue, CategoryMaxValue)
+	 VALUES ('$last_id', 'VictoryRatio', '1', '10')";
+	$sql->db_Query($query) or die ('Error, adding StatsCategories 3<br />'. mysql_error());
+ 	$query = 
+	"INSERT INTO ".TBL_STATSCATEGORIES."(Event, CategoryName, CategoryMinValue, CategoryMaxValue)
+	 VALUES ('$last_id', 'VictoryPercent', '1', '10')";
+	$sql->db_Query($query) or die ('Error, adding StatsCategories 4<br />'. mysql_error());
+ 	$query = 
+	"INSERT INTO ".TBL_STATSCATEGORIES."(Event, CategoryName, CategoryMinValue, CategoryMaxValue)
+	 VALUES ('$last_id', 'WinDrawLoss', '1', '10')";
+	$sql->db_Query($query) or die ('Error, adding StatsCategories 4<br />'. mysql_error());
+ 	$query = 
+	"INSERT INTO ".TBL_STATSCATEGORIES."(Event, CategoryName, CategoryMinValue, CategoryMaxValue)
+	 VALUES ('$last_id', 'UniqueOpponents', '1', '20')";
+	$sql->db_Query($query) or die ('Error, adding StatsCategories 5<br />'. mysql_error());
+ 	$query = 
+	"INSERT INTO ".TBL_STATSCATEGORIES."(Event, CategoryName, CategoryMinValue, CategoryMaxValue)
+	 VALUES ('$last_id', 'OpponentsELO', '1', '20')";
+	$sql->db_Query($query) or die ('Error, adding StatsCategories 6<br />'. mysql_error());
+ 	$query = 
+	"INSERT INTO ".TBL_STATSCATEGORIES."(Event, CategoryName, CategoryMinValue, CategoryMaxValue)
+	 VALUES ('$last_id', 'Streaks', '1', '20')";
+	$sql->db_Query($query) or die ('Error, adding StatsCategories 7<br />'. mysql_error());        	
+ 	$query = 
+	"INSERT INTO ".TBL_STATSCATEGORIES."(Event, CategoryName, CategoryMinValue, CategoryMaxValue)
+	 VALUES ('$last_id', 'Skill', '1', '20')";
+	$sql->db_Query($query) or die ('Error, adding StatsCategories 8<br />'. mysql_error());
+ 	$query = 
+	"INSERT INTO ".TBL_STATSCATEGORIES."(Event, CategoryName, CategoryMinValue, CategoryMaxValue)
+	 VALUES ('$last_id', 'Score', '1', '20')";
+	$sql->db_Query($query) or die ('Error, adding StatsCategories 9<br />'. mysql_error());
+ 	$query = 
+	"INSERT INTO ".TBL_STATSCATEGORIES."(Event, CategoryName, CategoryMinValue, CategoryMaxValue)
+	 VALUES ('$last_id', 'ScoreAgainst', '1', '20')";
+	$sql->db_Query($query) or die ('Error, adding StatsCategories 10<br />'. mysql_error());
+ 	$query = 
+	"INSERT INTO ".TBL_STATSCATEGORIES."(Event, CategoryName, CategoryMinValue, CategoryMaxValue)
+	 VALUES ('$last_id', 'ScoreDiff', '1', '20')";
+	$sql->db_Query($query) or die ('Error, adding StatsCategories 11<br />'. mysql_error());
+ 	$query = 
+	"INSERT INTO ".TBL_STATSCATEGORIES."(Event, CategoryName, CategoryMinValue, CategoryMaxValue)
+	 VALUES ('$last_id', 'Points', '1', '20')";
+	$sql->db_Query($query) or die ('Error, adding StatsCategories 12<br />'. mysql_error());
+		
+	for ($i=1;$i<=40; $i++)
+	{
+	   admin_update($sql -> db_Insert("user", "0, 'Player".$i."', 'Player".$i."',  '', '".md5("test")."', '$key', 'test@hotmail.com', 'mysig', '', '', '1', '".time()."', '".time()."', '".time()."', '0', '0', '0', '0', '0', '0', '0', '', '', '0', '0', 'Player".$i."', '', '', '', '".time()."', ''"), 'insert', USRLAN_70);
+       $user_id = mysql_insert_id();
+	   
+	   $query = 
+	   "INSERT INTO ".TBL_PLAYERS."(Event,User)
+	    VALUES ($last_id,$user_id)";
+	   $sql->db_Query($query) or die ('Error, adding player '.$user_id.' in event '.$last_id.'<br />'. mysql_error());
+	}	
+*/
+
 	$message = $text;
 	
 ?>
